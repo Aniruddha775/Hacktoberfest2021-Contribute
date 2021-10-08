66a55decf450f100e50ba5a12a41f679c337a800
Manasvi Prakash <79736693+manasvip01@users.noreply.github.com>
2021-10-09 00:36:43 +0530

    Update NoOfwordsinstrings.c

--- c/NoOfwordsinstrings.c
+++ c/NoOfwordsinstrings.c
@@ -1,25 +1,14 @@
-/* C Program to Count Total Number of Words in a String */
- 
-#include <stdio.h>
-#include <string.h>
- 
-int main()
-{
-  	char str[100];
-  	int i, totalwords;
-  	totalwords = 1;
- 
-  	printf("\n Please Enter any String :  ");
-  	gets(str);
-  	 	   	
-  	for(i = 0; str[i] != '\0'; i++)
-	{
-		if(str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
-		{
-			totalwords++;	
-		} 
-	}	
-	printf("\n The Total Number of Words in this String %s  = %d", str, totalwords);
-	
-  	return 0;
+#include<iostream>
+using namespace std;
+int main(){
+	string s;
+	cin>>s;
+	int i,c=0;
+	for(i=0;i<s.length();i++){
+		if(s[i]==' ')
+		continue;
+		c++;
+	}
+	cout<<c;
+	return 0;
 }
